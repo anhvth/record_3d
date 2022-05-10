f9fd9168c2eb25a7b76911f7ee49ebfe7f47f89e
anh vo <anhvth.226@gmail.com>
2022-05-10 14:15:56 +0700

    update

---
+++ demo-main.py
@@ -0,0 +1,204 @@
+import os
+import os.path as osp
+import time
+import numpy as np
+from record3d import Record3DStream
+import cv2
+from threading import Event
+import numpy as np
+import argparse
+parser = argparse.ArgumentParser()
+    # import cv2
+import mediapipe as mp
+
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+
+
+
+def lmk2np(lmks):
+    res = []
+    for lmk in lmks:
+        res.append([lmk.x, lmk.y, lmk.z])
+    return np.array(res)
+
+
+def mediapipe_facemesh_predict(image):
+
+    with mp_face_mesh.FaceMesh(
+            static_image_mode=True,
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5) as face_mesh:
+        results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        annotated_image = image.copy()
+        # Print and draw face mesh landmarks on the image.
+        if not results.multi_face_landmarks:
+            return annotated_image
+        for face_landmarks in results.multi_face_landmarks:
+            # lmk_np = lmk2np(face_landmarks.landmark)
+            # np.save(f'{args.out_np_dir}/{idx:06d}', lmk_np)
+            # if args.vis:
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style())
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_contours_style())
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_iris_connections_style())
+    return annotated_image
+
+
+
+def vis_3d(rgbd):
+    import open3d as o3d
+    global geometry, vis
+
+    rgb = rgbd[...,:3]/255.
+    de = rgbd[..., 3:]
+    # import ipdb; ipdb.set_trace()
+    color = o3d.cpu.pybind.geometry.Image(rgb)
+    de = 255/(de+1e-12)
+    depth = o3d.cpu.pybind.geometry.Image(de)
+    # depth_raw = o3d.io.read_image("../../test_data/RGBD/depth/00000.png")
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color, depth)#, 1, 50, False)
+
+    # with open(args.intrinsic, 'r') as f:
+    #     K = f.read().split()
+    fx,cx = 594.90771484,239.74267578
+    fy,cy = 594.90771484,319.23379517
+    
+    intrinsic = o3d.camera.PinholeCameraIntrinsic(height=480, width=640, fx=fx,fy=fy,cx=cx,cy=cy)
+    extrinsic = np.diag(np.array([-1, -1, -1, 1]))
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, extrinsic
+    )
+    # o3d.visualization.webrtc_server.enable_webrtc()
+    o3d.visualization.draw(pcd)
+    # vis.add_geometry(pcd)
+
+    #------
+    # import ipdb; ipdb.set_trace()
+    # geometry.points = pcd.points
+    # vis.update_geometry(geometry)
+    # vis.poll_events()
+    # vis.update_renderer()
+
+
+
+parser.add_argument('--dump', action='store_true', default=False)
+
+parser.add_argument('--rgbd_dir', default='data/rgbd/')
+args = parser.parse_args()
+if args.dump:
+    os.makedirs(args.rgbd_dir, exist_ok=True)
+class DemoApp:
+    def __init__(self):
+        self.event = Event()
+        self.session = None
+        self.DEVICE_TYPE__TRUEDEPTH = 0
+        self.DEVICE_TYPE__LIDAR = 1
+
+    def on_new_frame(self):
+        """
+        This method is called from non-main thread, therefore cannot be used for presenting UI.
+        """
+        self.event.set()  # Notify the main thread to stop waiting and process new frame.
+
+    def on_stream_stopped(self):
+        print('Stream stopped')
+
+    def connect_to_device(self, dev_idx):
+        print('Searching for devices')
+        devs = Record3DStream.get_connected_devices()
+        print('{} device(s) found'.format(len(devs)))
+        for dev in devs:
+            print('\tID: {}\n\tUDID: {}\n'.format(dev.product_id, dev.udid))
+
+        if len(devs) <= dev_idx:
+            raise RuntimeError('Cannot connect to device #{}, try different index.'
+                               .format(dev_idx))
+
+        dev = devs[dev_idx]
+        self.session = Record3DStream()
+        self.session.on_new_frame = self.on_new_frame
+        self.session.on_stream_stopped = self.on_stream_stopped
+        self.session.connect(dev)  # Initiate connection and start capturing
+
+    def get_intrinsic_mat_from_coeffs(self, coeffs):
+        return np.array([[coeffs.fx,         0, coeffs.tx],
+                         [        0, coeffs.fy, coeffs.ty],
+                         [        0,         0,         1]])
+
+
+    def start_processing_stream(self):
+        # intrinsic_mat = self.get_intrinsic_mat_from_coeffs(self.session.get_intrinsic_mat())
+        # print(intrinsic_mat)
+        while True:
+            self.event.wait()  # Wait for new frame to arrive
+
+            # Copy the newly arrived RGBD frame
+            depth = self.session.get_depth_frame()
+            rgb = self.session.get_rgb_frame()
+            intrinsic_mat = self.get_intrinsic_mat_from_coeffs(self.session.get_intrinsic_mat())
+            # You can now e.g. create point cloud by projecting the depth map using the intrinsic matrix.
+
+            # Postprocess it
+            # import ipdb; ipdb.set_trace()
+            depth[np.isnan(depth)] = 1000
+            depth[depth>1] = 255
+            # depth = 1/(depth+1e-6)
+            if self.session.get_device_type() == self.DEVICE_TYPE__TRUEDEPTH:
+                depth = cv2.flip(depth, 1)
+                rgb = cv2.flip(rgb, 1)
+
+            rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+
+            # Show the RGBD Stream
+
+            # import ipdb; ipdb.set_trace()
+            if args.dump == True:
+                t = time.time()
+                rgbd = np.concatenate([rgb, depth[...,None]], 2)
+                out_path = f'{args.rgbd_dir}/{t}'
+                print(out_path)
+                # np.save(out_path, rgbd, )
+                # np.save(f'{args.rgbd_dir}/last', rgbd, )
+                # print(t)
+                vis_3d(rgbd)
+                break
+            else:
+                rgb = np.rot90(rgb)
+                depth = np.rot90(depth)
+                rgb = mediapipe_facemesh_predict(rgb)
+                cv2.imshow('RGB', rgb)
+                cv2.imshow('Depth', depth)
+                cv2.waitKey(1)
+            self.event.clear()
+        #== End loop
+        # vis.destroy_window()
+
+
+if __name__ == '__main__':
+    app = DemoApp()
+    app.connect_to_device(dev_idx=0)
+    app.start_processing_stream()
+q
---
+++ record_data.py
@@ -0,0 +1,204 @@
+from loguru import logger
+import mmcv
+import os
+import os.path as osp
+import time
+import numpy as np
+from record3d import Record3DStream
+import cv2
+from threading import Event
+import numpy as np
+import argparse
+from datetime import datetime
+
+# parser = argparse.ArgumentParser()
+
+def lmk2np(lmks):
+    res = []
+    for lmk in lmks:
+        res.append([lmk.x, lmk.y, lmk.z])
+    return np.array(res)
+
+
+def mediapipe_facemesh_predict(image):
+
+    with mp_face_mesh.FaceMesh(
+            static_image_mode=True,
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5) as face_mesh:
+        results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        annotated_image = image.copy()
+        # Print and draw face mesh landmarks on the image.
+        if not results.multi_face_landmarks:
+            return annotated_image
+        for face_landmarks in results.multi_face_landmarks:
+            # lmk_np = lmk2np(face_landmarks.landmark)
+            # np.save(f'{args.out_np_dir}/{idx:06d}', lmk_np)
+            # if args.vis:
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style())
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_contours_style())
+            mp_drawing.draw_landmarks(
+                    image=annotated_image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_iris_connections_style())
+    return annotated_image
+
+def save_thread_function(img, depth,intrinsic_mat, name):
+    mmcv.imwrite(img, f'{name}.jpg')
+    mmcv.dump(dict(depth=depth, intrinsic_mat=intrinsic_mat), f'{name}.pkl')
+
+def vis_3d(rgbd):
+    import open3d as o3d
+    global geometry, vis
+
+    rgb = rgbd[...,:3]/255.
+    de = rgbd[..., 3:]
+    # import ipdb; ipdb.set_trace()
+    color = o3d.cpu.pybind.geometry.Image(rgb)
+    de = 255/(de+1e-12)
+    depth = o3d.cpu.pybind.geometry.Image(de)
+    # depth_raw = o3d.io.read_image("../../test_data/RGBD/depth/00000.png")
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color, depth)#, 1, 50, False)
+
+    # with open(args.intrinsic, 'r') as f:
+    #     K = f.read().split()
+    fx,cx = 594.90771484,239.74267578
+    fy,cy = 594.90771484,319.23379517
+    
+    intrinsic = o3d.camera.PinholeCameraIntrinsic(height=480, width=640, fx=fx,fy=fy,cx=cx,cy=cy)
+    extrinsic = np.diag(np.array([-1, -1, -1, 1]))
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, intrinsic, extrinsic
+    )
+    # o3d.visualization.webrtc_server.enable_webrtc()
+    o3d.visualization.draw(pcd)
+    # vis.add_geometry(pcd)
+
+    #------
+    # import ipdb; ipdb.set_trace()
+    # geometry.points = pcd.points
+    # vis.update_geometry(geometry)
+    # vis.poll_events()
+    # vis.update_renderer()
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('out_dir')
+parser.add_argument('-n', default=1000, type=int)
+# parser.add_argument('--arguments', '-a', nargs='+', default=None)
+args = parser.parse_args()
+
+
+class DemoApp:
+    def __init__(self):
+        self.event = Event()
+        self.session = None
+        self.DEVICE_TYPE__TRUEDEPTH = 0
+        self.DEVICE_TYPE__LIDAR = 1
+
+    def on_new_frame(self):
+        """
+        This method is called from non-main thread, therefore cannot be used for presenting UI.
+        """
+        self.event.set()  # Notify the main thread to stop waiting and process new frame.
+
+    def on_stream_stopped(self):
+        print('Stream stopped')
+
+    def connect_to_device(self, dev_idx):
+        print('Searching for devices')
+        devs = Record3DStream.get_connected_devices()
+        print('{} device(s) found'.format(len(devs)))
+        for dev in devs:
+            print('\tID: {}\n\tUDID: {}\n'.format(dev.product_id, dev.udid))
+
+        if len(devs) <= dev_idx:
+            raise RuntimeError('Cannot connect to device #{}, try different index.'
+                               .format(dev_idx))
+
+        dev = devs[dev_idx]
+        self.session = Record3DStream()
+        self.session.on_new_frame = self.on_new_frame
+        self.session.on_stream_stopped = self.on_stream_stopped
+        self.session.connect(dev)  # Initiate connection and start capturing
+
+    def get_intrinsic_mat_from_coeffs(self, coeffs):
+        return np.array([[coeffs.fx,         0, coeffs.tx],
+                         [        0, coeffs.fy, coeffs.ty],
+                         [        0,         0,         1]])
+
+
+    def start_processing_stream(self):
+        # intrinsic_mat = self.get_intrinsic_mat_from_coeffs(self.session.get_intrinsic_mat())
+        # print(intrinsic_mat)
+        now = datetime.now()
+        version = now.strftime("%b%d-%H:%M:%S")
+        version = version.replace(':', '-')
+
+        out_dir = osp.join(args.out_dir, version)
+        i = 0
+
+        cap = cv2.VideoCapture(0)
+        pbar = mmcv.ProgressBar(args.n)
+        import threading
+
+        thread_list = []
+        while True:
+            self.event.wait()  # Wait for new frame to arrive
+
+            # Copy the newly arrived RGBD frame
+            depth = self.session.get_depth_frame()
+            rgb = self.session.get_rgb_frame()
+            ret, webcam_bgr = cap.read()
+
+            intrinsic_mat = self.get_intrinsic_mat_from_coeffs(self.session.get_intrinsic_mat())
+
+            if self.session.get_device_type() == self.DEVICE_TYPE__TRUEDEPTH:
+                depth = cv2.flip(depth, 1)
+                rgb = cv2.flip(rgb, 1)
+
+            rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+            # import ipdb; ipdb.set_trace()
+            webcam_bgr = mmcv.imrescale(webcam_bgr, (480, 480))
+            # import ipdb; ipdb.set_trace()
+            pair = np.concatenate([rgb, webcam_bgr], 0)
+
+            name = f'{out_dir}/{i:06d}'
+            i+= 1
+
+            # save_thread_function
+            x = threading.Thread(target=save_thread_function, args=(pair, depth, intrinsic_mat, name))
+            x.start()            
+            thread_list.append(x)
+            self.event.clear()
+            pbar.update()
+            if i > args.n:
+                break
+        for _ in thread_list:
+            _.join()
+        logger.info('Finish')
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    app = DemoApp()
+    app.connect_to_device(dev_idx=0)
+    app.start_processing_stream()
